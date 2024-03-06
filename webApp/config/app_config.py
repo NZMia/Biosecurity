e135a393cbfd58f2476cadfb78361fa2e2f73dfe
NZMia <mia7zwy@hotmail.com>
2024-03-06 09:25:54 +0000

    config update

--- webApp/config/app_config.py
+++ webApp/config/app_config.py
@@ -8,7 +8,7 @@ class Config():
 
   SECRET_KEY = os.getenv('SECRET', 'get_secret_key')
 
-  MYSQL_HOST = os.getenv('MYSQL_HOST', 'nzmia.mysql.pythonanywhere-services.com')
-  MYSQL_USER = os.getenv('MYSQL_USER', 'nzmia')
+  MYSQL_HOST = os.getenv('MYSQL_HOST', 'zhangm3a.mysql.pythonanywhere-services.com')
+  MYSQL_USER = os.getenv('MYSQL_USER', 'zhangm3a')
   MYSQL_PASSWORD = os.getenv('MYSQL_PASSWORD', 'admin123#')
-  MYSQL_DB = os.getenv('MYSQL_DB', 'nzmia$biosecurity')
+  MYSQL_DB = os.getenv('MYSQL_DB', 'zhangm3a$biosecurity')
